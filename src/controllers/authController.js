87f9ef84ae5eaec49fb329326e10d65378550870
devasol <dawit8908@gmail.com>
2025-10-16 16:30:16 +0300

    Two factor authentication, qr-code generationhas built

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.js";
-
+import qrcode from "qrcode";
+import jwt from "jsonwebtoken";
+import speakeasy from "speakeasy";
 export const register = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -27,14 +29,66 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   console.log("Authenticated user is: ", req.user.username);
   res.status(200).json({
-    message: "get user successfully",
+    message: "User logged in successfully",
     username: req.user.username,
     isMfaActive: req.user.isMfaActive,
   });
 };
 
-export const authStatus = async (req, res) => {};
-export const logout = async (req, res) => {};
-export const setup2FA = async (req, res) => {};
+export const authStatus = async (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      message: "User logged in successfully",
+      username: req.user.username,
+      isMfaActive: req.user.isMfaActive,
+    });
+  } else {
+    res.status(401).json({
+      message: "Unauthorized user",
+    });
+  }
+};
+export const logout = async (req, res) => {
+  if (!req.user)
+    return res.status(401).json({
+      message: "Unauthorized user",
+    });
+  req.logout((err) => {
+    if (err)
+      return res.status(401).json({
+        message: "User not logged in",
+      });
+    res.status(200).json({
+      message: "Logout successfull",
+    });
+  });
+};
+export const setup2FA = async (req, res) => {
+  try {
+    console.log("The req.user is:", req.user);
+    const user = req.user;
+    var secret = speakeasy.generateSecret();
+    console.log("The secret key is:", secret);
+    user.twoFactorSecret = secret.base32;
+    user.isMfaActive = true;
+    await user.save();
+    const url = speakeasy.otpauthURL({
+      secret: secret.base32,
+      label: `${req.user.username}`,
+      issuer: "Dawit Solomon",
+      encoding: "base32",
+    });
+    const qrImageUrl = await qrcode.toDataURL(url);
+    res.status(200).json({
+      secret: secret.base32,
+      qrCode: qrImageUrl,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Error registering user.",
+      message: error,
+    });
+  }
+};
 export const verify2FA = async (req, res) => {};
 export const reset2FA = async (req, res) => {};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,18 +19,39 @@ router.post("/register", register);
 router.post("/login", passport.authenticate("local"), login);
 
 // Auth Status Route
-router.post("/status", authStatus);
+router.get("/status", authStatus);
 
 // Logout Route
 router.post("/logout", logout);
 
 // 2FA Route
-router.post("/2fa/setup", setup2FA);
+router.post(
+  "/2fa/setup",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: "Unauthorized" });
+  },
+  setup2FA
+);
 
 // Verify Route
-router.post("/2fa/verify", verify2FA);
+router.post(
+  "/2fa/verify",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: "Unauthorized" });
+  },
+  verify2FA
+);
 
 // Reset Route
-router.post("/2fa/reset", reset2FA);
+router.post(
+  "/2fa/reset",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: "Unauthorized" });
+  },
+  reset2FA
+);
 
 export default router;
