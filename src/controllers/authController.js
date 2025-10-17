123ba91c55568b6385b4e8a7d39c019105195268
devasol <dawit8908@gmail.com>
2025-10-17 15:52:56 +0300

    user 2FA verification and reset endpoints are done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -90,5 +90,47 @@ export const setup2FA = async (req, res) => {
     });
   }
 };
-export const verify2FA = async (req, res) => {};
-export const reset2FA = async (req, res) => {};
+export const verify2FA = async (req, res) => {
+  const { token } = req.body;
+  const user = req.user;
+
+  const verified = speakeasy.totp.verify({
+    secret: user.twoFactorSecret,
+    encoding: "base32",
+    token,
+  });
+
+  if (verified) {
+    const jwtToken = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1hr",
+      }
+    );
+    res.status(200).json({
+      message: "2FA successfull",
+      token: jwtToken,
+    });
+  } else {
+    res.status(400).json({
+      message: "Invalid 2FA token",
+    });
+  }
+};
+export const reset2FA = async (req, res) => {
+  try {
+    const user = req.user;
+    user.isMfaActive = false;
+    user.twoFactorSecret = "";
+
+    await user.save();
+    res.status(200).json({
+      message: "@2FA reset successfull",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error reseting 2Fa ${error}`,
+    });
+  }
+};
